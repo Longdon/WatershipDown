dbb16fe2863d11be553d9a9dbe74a567fc77a31e
cr <android_dev@chrock.de>
2012-12-05 15:04:24 +0100

    init

---
+++ WatershipDown/gen/de/rockschenn/android/games/watershipdown/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.rockschenn.android.games.watershipdown;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
